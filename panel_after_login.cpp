d347e40bde1469d7f25b032e4560cc5251173041
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-03-13 21:36:56 +0700

    added loop for multiple-time use

--- panel_after_login.cpp
+++ panel_after_login.cpp
@@ -13,41 +13,45 @@ void idle_login_panel(int rank, bool &exit, Student *student, Staff *staff, int 
     {
         int command;
         cout << "1. profile\n2. logout\n3.exit\n" << "input command: "; cin >> command;
-        if (command == 1)
+        while (command)
         {
-            logout = true;
-            return;
-        }
-        if (command == 2)
-        {
-            if (rank == 1)
+            cout << "1. profile\n2. logout\n3.exit\n" << "input command: "; cin >> command;
+            if (command == 1)
             {
-                
-                cout << "ID: " << student[ID] . Student_ID << endl;
-                cout << "Year Studied" << student[ID] . Year_Studied << endl;
-                cout << "Password: " << student[ID] . Password << endl;
-                cout << "LastName: " << student[ID] . LastName << endl;
-                cout << "FirstName" << student[ID] . FirstName << endl;
-                cout << "Female: " << student[ID] . FemaleGender << endl;
-                cout << "DD: " << student[ID] . DD << endl;
-                cout << "MM: " << student[ID] . MM << endl;
-                cout << "YY: " << student[ID] . YY << endl;
-                cout << "Social ID: " << student[ID] . Social_ID << endl;
-                cout << "Class: " << student[ID] . Class_ID << endl;
-                
+                logout = true;
+                return;
             }
-            else if (rank == 2)
+            if (command == 2)
             {
-                cout << "ID: " << staff[ID] . Staff_ID << endl;
-                cout << "Password: " << staff[ID] . Password << endl;
-                cout << "LastName: " << staff[ID] . LastName << endl;
-                cout << "FirstName: " << staff[ID] . FirstName << endl;
+                if (rank == 1)
+                {
+                    
+                    cout << "ID: " << student[ID] . Student_ID << endl;
+                    cout << "Year Studied" << student[ID] . Year_Studied << endl;
+                    cout << "Password: " << student[ID] . Password << endl;
+                    cout << "LastName: " << student[ID] . LastName << endl;
+                    cout << "FirstName" << student[ID] . FirstName << endl;
+                    cout << "Female: " << student[ID] . FemaleGender << endl;
+                    cout << "DD: " << student[ID] . DD << endl;
+                    cout << "MM: " << student[ID] . MM << endl;
+                    cout << "YY: " << student[ID] . YY << endl;
+                    cout << "Social ID: " << student[ID] . Social_ID << endl;
+                    cout << "Class: " << student[ID] . Class_ID << endl;
+                    
+                }
+                else if (rank == 2)
+                {
+                    cout << "ID: " << staff[ID] . Staff_ID << endl;
+                    cout << "Password: " << staff[ID] . Password << endl;
+                    cout << "LastName: " << staff[ID] . LastName << endl;
+                    cout << "FirstName: " << staff[ID] . FirstName << endl;
+                }
             }
-        }
-        else if (command == 3)
-        {
-            exit = true;
-            return;
+            else if (command == 3)
+            {
+                exit = true;
+                return;
+            }
         }
     }
     
