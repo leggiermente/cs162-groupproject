52904f01ac5c1365c8ad00e73d058fe92c52aab0
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-03-19 22:15:21 +0700

    student.txt

---
+++ importStudentToCourse.cpp
@@ -0,0 +1,25 @@
+#include "Struct.h"
+
+#include <fstream>
+
+void ImportStudentsToCoursesInSemester(Student *students, int numStu, Course thisCourse)
+{
+    thisCourse.listStudentInCourse = students;
+    for (int i = 0; i < numStu; i++)
+    {
+        students[i]
+    }
+    std::ifstream fin;
+    std::ofstream fout;
+    for (int i = 0; i < numStu; i++)
+    {
+        fin.open(students->studentID + ".txt");
+        fout.open(students->studentID + "2.txt");
+        string line;
+        getline(fin, line);
+        fout << line << endl;
+        fout << thisCourse.ID << endl;
+        fin.close();
+        fout.close();
+    }
+}
