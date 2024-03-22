8638bc2dfb2a937df3656127b0b377a7202cb3b8
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-03-22 12:15:51 +0700

    fix naming, add first name and last name

--- addStudentToCouse.cpp
+++ addStudentToCouse.cpp
@@ -1,16 +1,18 @@
 #include <fstream>
+#include "Struct.h"
+#include "Student.h"
 
 using namespace std;
 void addStudentToCourse(Course thisCourse, Student *student, int numStu)
 {
     thisCourse.listStudentInCourse = student;
     ofstream fout;
-    fout.open(thisCourse.courseID + ".csv");
+    fout.open(thisCourse.ID + ".csv");
     fout << numStu << " : number of students in this course\n";
-    fout << "Student ID, Student Name, Total Mark, Final Mark, Midterm Mark, Other Mark\n";
+    fout << "Student ID, First Name, Last Name, Total Mark, Final Mark, Midterm Mark, Other Mark\n";
     for (int i = 0; i < numStu; i++)
     {
-       fout << student[i].studentID << ", " << student[i].studentName << "\n";
+       fout << student[i].Student_ID << ", " << student[i].FirstName << ", " << student[i].LastName << "\n";
     }
     fout.close();
 }
