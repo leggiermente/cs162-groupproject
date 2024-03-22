a742e49ca1e02821466e7c9de3d11820599f0b54
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-03-22 12:05:06 +0700

    add students to course csv file

--- addStudentToCouse.cpp
+++ addStudentToCouse.cpp
@@ -1,4 +1,15 @@
+#include <fstream>
+
+using namespace std;
 void addStudentToCourse(Course thisCourse, Student *student, int numStu)
 {
     thisCourse.listStudentInCourse = student;
+    ofstream fout;
+    fout.open(thisCourse.courseID + ".csv");
+    fout << numStu << " : number of students in this course\n";
+    fout << "Student ID, Student Name, Total Mark, Final Mark, Midterm Mark, Other Mark\n";
+    for (int i = 0; i < numStu; i++)
+    {
+       fout << student[i].studentID << ", " << student[i].studentName << "\n";
+    }
 }
