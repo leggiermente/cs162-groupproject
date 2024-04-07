37dcc6b1aa096d65a530875b73bbffd0366d1979
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-04-07 18:20:25 +0700

    update adding course to student

--- Struct.h
+++ Struct.h
@@ -1,49 +1,68 @@
 #ifndef STRUCT_H_INCLUDED
 #define STRUCT_H_INCLUDED
-
 #include <string>
 using namespace std;
-struct Student;
+
+// Temeporary struct for Score
+struct ScoreStu {
+    string courseID;
+    string semester;
+    string year;
+    float otherMark;
+    float midMark;
+    float finalMark;
+    float totalMark;
+};
 struct Class;
-struct schoolYear;
+struct SchoolYear;
 struct Course;
 struct Semester;
-struct Student{
-    int studentID=0;
-    int semesterStudied=0;
-    string password="";
-    string firstName="",lastName="";
-    bool femaleGender=0;
-    int DD=0,MM=0,YY=0;
-    int socialID=0;
-    double overallGPA=0; //updating overallGPA in student.csv
-    Class* classOfStudent;
+struct Student;
+struct Class {
+    string classID = "";
+    int yearStudied = 0;
+    int numStudent = 0; //updating number of Student in a class.
+    Student* listStudent = nullptr;
 };
-struct Class{
-    string classID="";
-    int yearStudied=0;
-    int numStudent=0; //updating number of Student in a class.
-    Student* listStudent;
+struct SchoolYear {
+    string period = "";
+    //int classCount = 0; //count the number of class in a school year.
+    //Class* classList;
+    int numSemester = 0;
+    Semester* listSemester = nullptr;
 };
-struct schoolYear{
-    string period="";
-    int classCount=0; //count the number of class in a school year.
-    Class* classList;
+struct Course {
+    string ID = "";
+    string courseName = "";
+    string className = "";
+    string teacher = "";
+    string dayOfWeek = "";
+    string session = "";
+    int numCredits = 0; 
+    int maxStudents = 50; 
+    int currStudents = 43;
+    Student** listStudentInCourse = nullptr;
 };
-struct Course{
-    string ID="";
-    string courseName="";
-    string className="";
-    string teacher="";
-    int numCredits=0;
-    int maxStudents=50;
-    int numStu = 0;
-    Student* listStudentInCourse;
+struct Semester {
+    int numSemesterInSchoolYear = 0;
+    string startDate = "";
+    string endDate = "";
+    int numCourses = 0;
+    Course* coursesListInSemester = nullptr;
 };
-struct Semester{
-    int numSemesterInSchoolYear=0;
-    int startDate=0,startMonth=0,startYear=0;
-    int endDate=0,endMonth=0,endYear=0;
-    Course *coursesListInSemester;
+struct Student {
+    string studentID = "";
+    int semesterStudied = 0;
+    string password = "";
+    string firstName = "", lastName = "";
+    bool femaleGender = 0;
+    string dob = "";
+    //int DD = 0, MM = 0, YY = 0;
+    string socialID = "";
+    double overallGPA = 0; //updating overallGPA in student.csv
+    int numCourse = 0;
+    ScoreStu* score = nullptr;
+    Course* courseOfStudent = nullptr;
 };
+
 #endif // STRUCT_H_INCLUDED
--- importStudentToCourse.cpp
+++ importStudentToCourse.cpp
@@ -2,13 +2,22 @@
 
 #include <fstream>
 
-void addToCoursesInSemesterToStudentsFiles(Student *students, int numStu, Course thisCourse)
+void addToCoursesInSemesterToStudents(Student *students, int numStu, Course thisCourse)
 {
-    thisCourse.listStudentInCourse = students;
-    for (int i = 0; i < numStu; i++)
+    for (int i = 0; i < numStu; ++i)
     {
-        students[i];
+        students[i].numCourse++;
+        Course* temp = new Course[students[i].numCourse];
+        for (int j = 0; j < students[i].numCourse - 2; ++j)
+        {
+            temp[j] = students[i].courseOfStudent[j];
+        }
+        temp[students[i].numCourse - 1] = thisCourse;
+        students[i].courseOfStudent = temp;
+        delete [] temp;
     }
+    
+    // add to student files
     std::ifstream fin;
     std::ofstream fout;
     for (int i = 0; i < numStu; i++)
@@ -21,9 +30,9 @@ void addToCoursesInSemesterToStudentsFiles(Student *students, int numStu, Course
             getline(fin, line);
             fout << line << endl;
         }
-        int numCourse;
-        fin >> numCourse;
-        for (int i = 0; i < numCourse; ++i)
+        int num = students[i].numCourse;
+        fin >> line; //ignore the nummber of courses, already in the RAM
+        for (int i = 0; i < num; ++i)
         {
             fout << thisCourse.ID << endl;
         }
@@ -33,7 +42,11 @@ void addToCoursesInSemesterToStudentsFiles(Student *students, int numStu, Course
         }
         fin.close();
         fout.close();
-        delete(students->studentID + ".txt");
-        rename(students->studentID + "2.txt", students->studentID + ".txt");
+        std::string oldFile = students->studentID + ".txt";
+        std::string newFile = students->studentID + "2.txt";
+        std::string renamedFile = students->studentID + ".txt";
+
+        remove(oldFile.c_str());
+        rename(newFile.c_str(), renamedFile.c_str());
     }
 }
