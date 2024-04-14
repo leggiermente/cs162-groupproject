4f50219462f480fc693975ed406fe69bd9cb5af4
ngbao12 <ngocbao6422@gmail.com>
2024-04-15 00:39:07 +0700

    declare

--- importScore.h
+++ importScore.h
@@ -9,5 +9,6 @@
 using namespace std;
 
 void readScoreBoardCSV(string filename, Course &course);
+void updateStudentResult(Student* scoreboard,int numStu ,float otherMark, float midMark, float finalMark, float totalMark, string studentID);
 
 #endif
