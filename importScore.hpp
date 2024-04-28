19e0d6dd2ffcb96da7c8662407e412961b2d5c72
ngbao12 <ngocbao6422@gmail.com>
2024-04-29 01:46:45 +0700

    fix

---
+++ importScore.hpp
@@ -0,0 +1,13 @@
+#ifndef importScore_hpp
+#define importScore_hpp
+
+#include "Struct.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void importScore(string path, Course &course);
+
+#endif /* importScore_hpp */
