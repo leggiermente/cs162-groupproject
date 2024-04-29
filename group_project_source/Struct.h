0351718e62acbf52e21bbe3959ffaef75e460561
leggiermente <23125070@student.hcmus.edu.vn>
2024-04-29 13:22:42 +0700

    comment for struct's variable

--- group_project_source/Struct.h
+++ group_project_source/Struct.h
@@ -8,52 +8,52 @@ struct Course;
 struct Semester;
 struct ScoreStu;
 struct Student{
-    string studentID="";
-    int semesterStudied=0;
-    string password="";
-    string firstName="",lastName="";
-    bool femaleGender=0;
-    string DD="",MM="",YY="";
+    string studentID=""; //ID of the student
+    int semesterStudied=0; //number of semester that a student has studied
+    string password=""; //password of the student's account
+    string firstName="",lastName=""; //name of the student
+    bool femaleGender=0; //sexuality of the student
+    string DD="",MM="",YY=""; //Birth date.
     string socialID="";
     double overallGPA=0; //updating overallGPA in student.csv
-    int numCourses=0;
-    ScoreStu* scoreOfStudent=nullptr;
+    int numCourses=0; //total number of courses that a student has learned from the 1st year.
+    ScoreStu* scoreOfStudent=nullptr; //array saves score of a student
 };
 struct Class{
-    string classID="";
-    int yearStudied=0;
+    string classID=""; //ID of the class
+    int yearStudied=0; //state of the class, 1-st year, 2-nd year,...
     int numStudent=0; //updating number of Student in a class.
-    Student* listStudent=nullptr;
+    Student* listStudent=nullptr; //list of student in the class
 };
 struct schoolYear{
-    string period="";
+    string period=""; //period name of the school year, 2023-2024,...
     int semCount=0; //count the number of semester in a school year.
     Semester *sem=nullptr;
 };
 struct Course{
-    string ID="";
-    string courseName="";
-    string className="";
-    string teacher="";
-    string dayoftheWeek="";
-    string sessionTime="";
+    string ID=""; //ID of a course
+    string courseName=""; //name of the course
+    string className=""; //name for the class
+    string teacher=""; //name of the teacher
+    string dayoftheWeek=""; //day of the week, followed the format
+    string sessionTime=""; //time session, followed the format
     int numCredits=0;
     int maxStudents=50;
     int numStudents=0;
-    Student** listStudentInCourse=nullptr;
+    Student** listStudentInCourse=nullptr; //list of the student studied in this course
 };
 struct Semester{
-    int numSemesterInSchoolYear=0;
+    int numSemesterInSchoolYear=0; //number of semester in the school year
     int startDate=0,startMonth=0,startYear=0;
     int endDate=0,endMonth=0,endYear=0;
-    int numCourses=0; //update numCourses
-    Course *coursesListInSemester=nullptr;
+    int numCourses=0; //number of courses in the semester
+    Course *coursesListInSemester=nullptr; //list of all courses in the semester
 };
 struct ScoreStu{
     string courseID="";
     string semester="";
     string year="";
-    float quiz=0;
+    float quiz=0; //point of quiz, mid, final, total
     float mid=0;
     float fin=0;
     float total=0;
